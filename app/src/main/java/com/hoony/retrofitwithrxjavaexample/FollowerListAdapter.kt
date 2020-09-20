9ffc23ee45116074660413cff86c23528b868414
thkim9373 <thkim9373@gmail.com>
2020-09-20 18:22:47 +0900

    Save commit.

---
+++ app/src/main/java/com/hoony/retrofitwithrxjavaexample/FollowerListAdapter.kt
@@ -0,0 +1,43 @@
+package com.hoony.retrofitwithrxjavaexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hoony.retrofitwithrxjavaexample.databinding.ItemFollowerBinding
+import com.hoony.retrofitwithrxjavaexample.retrofit.GithubUser
+
+class FollowerListAdapter(private val githubFollowerList: List<GithubUser>): ListAdapter<GithubUser, FollowerListAdapter.FollowerViewHolder>(
+    object : DiffUtil.ItemCallback<GithubUser>() {
+        override fun areItemsTheSame(oldItem: GithubUser, newItem: GithubUser): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: GithubUser, newItem: GithubUser): Boolean =
+            oldItem.hashCode() == newItem.hashCode()
+    }
+) {
+    override fun getItemCount(): Int = githubFollowerList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+        return FollowerViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_follower,
+                parent,
+                false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
+        holder.bind(githubFollowerList[position])
+    }
+
+    inner class FollowerViewHolder(private val binding: ItemFollowerBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(githubUser: GithubUser) {
+            binding.gitHubUser = githubUser
+        }
+    }
+}
